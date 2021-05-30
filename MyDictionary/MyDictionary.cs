2a8b2bb537502575016d7d3f769dbc4b2024dac2
Görkem Akan <gorkemakan123@gmail.com>
2021-05-30 22:49:43 +0300

    Day 4

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                values[j] = tempValues[j];
+            }
+            values[values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public V[] Values
+        {
+            get { return values; }
+        }
+        public K[] Keys
+        {
+            get { return keys; }
+        }
+
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cars = new MyDictionary<int, string> ();
+
+            cars.Add(1, "BMW");
+            cars.Add(2, "Mercedes");
+            cars.Add(3, "Audi");
+            cars.Add(4, "Tofaş");
+            cars.Add(5, "Volkswagen");
+
+            Console.WriteLine("Total car brand: " + cars.Count);
+
+            foreach (var car in cars.Values)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
