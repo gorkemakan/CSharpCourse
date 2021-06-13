32f3e75599183c1b9fc80304afec7127f3270cb2
Görkem Akan <gorkemakan123@gmail.com>
2021-06-13 16:11:05 +0300

    Day 7

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description + " Yıl: " + car.ModelYear + " Günlük fiyatı: " + car.DailyPrice);
+            }
+        }
+    }
+}
